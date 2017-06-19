a552587cc3d662086549daa57f15fca9d643bd63
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-06-19 08:11:23 +0530

    Contextually notify user about an initial commit
    "git status" indicated "Initial commit" when HEAD points at
    an unborn branch.  This message is shared with the commit
    log template "git commit" prepares for the user when
    creating a commit (i.e. "You are about to create the initial
    commit"), and is OK as long as the reader is aware of the
    nature of the message (i.e. it guides the user working
    toward the next commit), but was confusing to new users,
    especially the ones who do "git commit -m message" without
    having a chance to pay attention to the commit log template.
    
    The "Initial commit" indication wasn't an issue in the commit
    template. Taking that into consideration, a good solution would
    be to contextually use different messages to indicate the user
    that there were no commits in this branch.
    
    A few alternatives considered were,
    
    * Waiting for initial commit
    * Your current branch does not have any commits
    * Current branch waiting for initial commit
    
    Patch-derived-from: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wt-status.c
+++ wt-status.c
@@ -1578,7 +1578,10 @@ static void wt_longstatus_print(struct wt_status *s)
 
 	if (s->is_initial) {
 		status_printf_ln(s, color(WT_STATUS_HEADER, s), "%s", "");
-		status_printf_ln(s, color(WT_STATUS_HEADER, s), _("Initial commit"));
+		status_printf_ln(s, color(WT_STATUS_HEADER, s),
+				 s->commit_template
+				 ? _("Initial commit")
+				 : _("No commits yet"));
 		status_printf_ln(s, color(WT_STATUS_HEADER, s), "%s", "");
 	}
 
